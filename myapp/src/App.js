2bd775dd53be4313d384ea6f0df27a65bbf55ac4
Hiruni Ranasinghe <139541233+HiruniRanasinghe@users.noreply.github.com>
2024-11-29 16:36:24 +0000

    change App.js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-do
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import Contactus from './components/Contactus';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
 
 function App() {
@@ -14,12 +16,15 @@ function App() {
             <li><Link to="/home">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/login">Login</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path='/home' element={<Home/>}/>
           <Route path='/about' element={<AboutUs/>}/>
           <Route path='/contact' element={<Contactus/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/dash' element={<Dashboard/>}/>
         </Routes>
       </Router>
     </div>
