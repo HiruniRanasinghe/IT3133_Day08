8d67935aba052c87508dab729867bd611ed13684
Hiruni Ranasinghe <139541233+HiruniRanasinghe@users.noreply.github.com>
2024-11-29 15:44:24 +0000

    change

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Routes>
           <Route path='/home' element={<Home/>}/>
           <Route path='/about' element={<AboutUs/>}/>
-          <Route path='/home' element={<Contactus/>}/>
+          <Route path='/contact' element={<Contactus/>}/>
         </Routes>
       </Router>
     </div>
